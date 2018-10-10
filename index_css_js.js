baa7b5d5d517ab2db4937726efc82a2e9ceb598c
Diego Giuliani <giuliani.diego@gmail.com>
2018-10-10 08:22:32 -0300

    Added border and background to hex values

--- index_css_js.js
+++ index_css_js.js
@@ -12,7 +12,12 @@ function updateBackground(){
 }
 
 function updateHexCode() {
-	document.querySelector("#h2").innerHTML = color1.value + " " + color2.value
+	updateValue(document.querySelector('#value1'), color1.value);
+	updateValue(document.querySelector('#value2'), color2.value);	
+}
+function updateValue(element, value){
+	element.textContent = value;	
+	element.setAttribute('style','color:' + value);
 }
 
 document.addEventListener('DOMContentLoaded', function(){
